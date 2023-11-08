cc04cdbad23c92546e5eff2405dabfe8178a916a
Rafael <85527507+rgno1@users.noreply.github.com>
2023-11-08 18:55:05 -0300

    minor change

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,6 +11,7 @@ from app.database import get_db
 from app.database import Base
 
 
+
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
